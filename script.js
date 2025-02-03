0979972683031889dd12260006dacadf589bc661
Ankitsgh1234 <ankit2005sgh@gmail.com>
2025-02-03 22:24:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,9 @@ const playMusic = (track, pause = false) => {
 }
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`/songs/`)
+    // let a = await fetch(`/songs/`)
+    let a = await fetch(`/songs/${folder}/info.json`);
+
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
